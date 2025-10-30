2e3948c46c26e2cf5b0e4c7b2cb013d275d45f33
pvasbob <pvasbob@gmail.com>
2025-10-30 11:21:33 -0400

    add log in

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {runMain} from "node:module";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
 
 export const meta = () => ([
   {title: 'Resumind | Auth'},
@@ -8,6 +10,15 @@ export const meta = () => ([
 
 
 const Auth = () => {
+  const {isLoading, auth} = usePuterStore();
+  const location = useLocation();
+  const next = location.search.split('next=')[1];
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    if (auth.isAuthenticated) navigate(next);
+  }, [auth.isAuthenticated, next])
 
   return (
     <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
@@ -16,6 +27,31 @@ const Auth = () => {
           <div className="flex flex-col items-center gap-2 text-center">
             <h1>Welcome</h1>
             <h2>Log In to Contiune Your Job Journey</h2>
+          </div>
+          <div>
+            {isLoading ? (
+              <button className="auth-button animate-pulse">
+                <p>signing you in ...</p>
+              </button>
+            ) : (
+              <>
+                {auth.isAuthenticated ? (
+                  <button className="auth-button" onClick={auth.signOut}>
+                    <p>
+                      Log Out
+                    </p>
+                  </button>
+                ) : (
+
+                  <button className="auth-button" onClick={auth.signIn}>
+                    <p>
+                      Log In
+                    </p>
+                  </button>
+                )}
+
+              </>
+            )}
           </div>
 
         </section>
